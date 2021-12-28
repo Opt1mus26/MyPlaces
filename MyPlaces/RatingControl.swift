6c1890c1eef7e77f99aa965fbb422fe71a106373
Pavel <92750573+Opt1mus26@users.noreply.github.com>
2021-12-28 21:39:10 +0300

    add file rating control

---
+++ MyPlaces/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  MyPlaces
+//
+//  Created by Pavel Tsyganov on 28.12.2021.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
